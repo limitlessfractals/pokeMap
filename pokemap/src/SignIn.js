ad3f5e30d490951442b4dff10eae2b0506c400d0
limitlessfractals <fanwuq@gmail.com>
2018-01-28 18:41:15 -0500

    SignIn password and button

--- pokemap/src/SignIn.js
+++ pokemap/src/SignIn.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import {View, Text, ImageBackground} from 'react-native';
-import {Form, Item, Label, Input} from 'native-base';
+import {Form, Item, Label, Input, Button} from 'native-base';
 
 var myBackground = require('../assets/landing.jpg');
 
 class SignIn extends React.Component{
    state = {
-      email: ""
+      email: "",
+      password: ""
+   }
+   login = () =>{
+
    }
    render(){
       return(
          <View style={{flex: 1}}>
             <ImageBackground source={myBackground} style={styles.background}>
-               <View style={{}}>
+               <View style={styles.inputStyle}>
                   <Form>
                      <Item floatingLabel>
                         <Label>Email</Label>
                         <Input
-                           style={{}}
                            autoCorrect={false}
                            onChangeText={(email)=>this.setState({email})}
                         />
                      </Item>
+                     <Item floatingLabel>
+                        <Label>Password</Label>
+                        <Input
+                           autoCorrect={false}
+                           onChangeText={(password)=>this.setState({password})}
+                           secureTextEntry
+                        />
+                     </Item>
                   </Form>
+                  <View style={{marginTop: 10}}>
+                     <Button
+                        primary
+                        block
+                        onPress={this.login}
+                     >
+                        <Text style={styles.buttonText}>Sign In/Sign Up</Text>
+                     </Button>
+                  </View>
                </View>
             </ImageBackground>
          </View>
@@ -35,6 +55,16 @@ const styles = {
       flex: 1,
       width: null,
       height: null
+   },
+   inputStyle: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      margin: 10
+   },
+   buttonText: {
+      color: 'white',
+      fontWeight: 'bold'
    }
 }
 
