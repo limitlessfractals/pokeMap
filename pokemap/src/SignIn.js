f9ba925089d95866cea6142bcf8071d70b638f34
limitlessfractals <fanwuq@gmail.com>
2018-01-28 18:26:08 -0500

    SignIn email input

--- pokemap/src/SignIn.js
+++ pokemap/src/SignIn.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground} from 'react-native';
+import {Form, Item, Label, Input} from 'native-base';
+
+var myBackground = require('../assets/landing.jpg');
 
 class SignIn extends React.Component{
+   state = {
+      email: ""
+   }
    render(){
       return(
-         <View>
-            <Text>Sign in screen</Text>
+         <View style={{flex: 1}}>
+            <ImageBackground source={myBackground} style={styles.background}>
+               <View style={{}}>
+                  <Form>
+                     <Item floatingLabel>
+                        <Label>Email</Label>
+                        <Input
+                           style={{}}
+                           autoCorrect={false}
+                           onChangeText={(email)=>this.setState({email})}
+                        />
+                     </Item>
+                  </Form>
+               </View>
+            </ImageBackground>
          </View>
       )
+   }
+}
+
+const styles = {
+   background: {
+      flex: 1,
+      width: null,
+      height: null
    }
 }
 
