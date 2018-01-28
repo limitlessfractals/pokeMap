c1e5f75dc4e03a3fc1d0ed42d1d4dc5f219fbd1d
limitlessfractals <fanwuq@gmail.com>
2018-01-27 20:07:15 -0500

    SignIn.js

---
+++ pokemap/src/SignIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class SignIn extends React.Component{
+   render(){
+      return(
+         <View>
+            <Text>Sign in screen</Text>
+         </View>
+      )
+   }
+}
+
+export default SignIn;
